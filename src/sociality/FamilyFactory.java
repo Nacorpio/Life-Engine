b31a4db9b5d1b1bab68aeb7f6e8f599cd8f85328
Nacorpio <teampavilion@gmail.com>
2013-06-11 10:14:12 +0200

    Added comments
    I added some comments to the family factory so that the API is easier
    to use.

--- src/sociality/FamilyFactory.java
+++ src/sociality/FamilyFactory.java
@@ -4,6 +4,14 @@ import body.Human;
 
 public class FamilyFactory {
 
+	/**
+	 * Creates a new family with the specified name and members.
+	 * @param familyName The name of the family.
+	 * @param father The father of the family.
+	 * @param mother The mother of the family.
+	 * @param siblings The siblings of the family.
+	 * @return Returns the new family.
+	 */
 	public final static Family createFamily(String familyName, Human father, Human mother, Human... siblings){
 		return new Family(familyName, mother, mother, siblings);
 	}
